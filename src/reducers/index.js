86f760723dc41fcada942af459e1d9d041534f9e
Victor Ciornii <ciorniivictor96@gmail.com>
2021-10-08 00:58:27 +0300

    local storage

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,26 @@ import booksReducer from './booksReducer';
 import termReducer from './termReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { loadStateFromLocalStorage, saveStateToLocalStorage } from '../localStorage';
+import throttle from 'lodash/throttle';
 
 const rootReducer = combineReducers({
   books: booksReducer,
   term: termReducer,
 });
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistedState = loadStateFromLocalStorage();
+
+export const store = createStore(
+  rootReducer,
+  persistedState,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
+
+store.subscribe(
+  throttle(() => {
+    saveStateToLocalStorage(store.getState());
+  }, 1000),
+);
+
+export default store;
