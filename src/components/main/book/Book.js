0be13f8cb947f90277b186b23e0185745023727f
Victor Ciornii <ciorniivictor96@gmail.com>
2021-10-08 12:26:08 +0300

    don't show empty books

--- src/components/main/book/Book.js
+++ src/components/main/book/Book.js
@@ -6,31 +6,35 @@ import { getLimitedSymbol } from '../../../utils';
 const Book = props => {
   const book = props.book;
 
-  return (
-    <NavLink className='book' to={`/detailed/${book.id}`}>
-      <div className='book__title'>{book?.volumeInfo?.title}</div>
-      <div className='book__img'>
-        {book?.volumeInfo?.imageLinks?.thumbnail && (
-          <img
-            src={book?.volumeInfo?.imageLinks?.thumbnail}
-            alt={book?.volumeInfo?.title}
-            width='128'
-            height='169'
-            loading='lazy'
+  if (book.volumeInfo.description === undefined && book.volumeInfo.imageLinks === undefined) {
+    return '';
+  } else {
+    return (
+      <NavLink className='book' to={`/detailed/${book.id}`}>
+        <div className='book__title'>{book?.volumeInfo?.title}</div>
+        <div className='book__img'>
+          {book?.volumeInfo?.imageLinks?.thumbnail && (
+            <img
+              src={book?.volumeInfo?.imageLinks?.thumbnail}
+              alt={book?.volumeInfo?.title}
+              width='128'
+              height='169'
+              loading='lazy'
+            />
+          )}
+        </div>
+        <div className='book__subtitle'>{book?.volumeInfo?.subtitle}</div>
+        {book.volumeInfo?.description && (
+          <div
+            className='book__desc'
+            dangerouslySetInnerHTML={{
+              __html: getLimitedSymbol(`${book.volumeInfo?.description}`),
+            }}
           />
         )}
-      </div>
-      <div className='book__subtitle'>{book?.volumeInfo?.subtitle}</div>
-      {book.volumeInfo?.description && (
-        <div
-          className='book__desc'
-          dangerouslySetInnerHTML={{
-            __html: getLimitedSymbol(`${book.volumeInfo?.description}`),
-          }}
-        />
-      )}
-    </NavLink>
-  );
+      </NavLink>
+    );
+  }
 };
 
 export default Book;
